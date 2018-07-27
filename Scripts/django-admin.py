84d17103c48b1ec30ba5f5bc939258e0b2476a96
miner46er <stefanus.ardimulia@gmail.com>
2018-07-27 11:03:21 +0700

    First commit all files

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\stefa.desktop-incffvo\desktop\learn\python\django\learn\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
